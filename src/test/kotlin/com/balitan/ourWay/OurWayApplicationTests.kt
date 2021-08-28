93921f9ebf686fbfad084945701eac470476d25c
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2021-08-28 22:35:33 +0530

    Initial commit

---
+++ src/test/kotlin/com/balitan/ourWay/OurWayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.balitan.ourWay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OurWayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
